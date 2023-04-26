5af1375a46d0be23f963f975af356efd3339193c
Anass ibnouali <annass.ibnouali@gmail.com>
2023-04-26 17:50:36 +0100

    initial commit

---
+++ src/test/java/com/craft/orientdb/OrientdbJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.craft.orientdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrientdbJavaApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
